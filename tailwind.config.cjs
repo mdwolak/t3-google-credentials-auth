8c94c47fe86217434b4363e3961212f9f5fd226a
mdwolak <mdwolak@googlemail.com>
2023-07-10 23:28:18 +0100

    Added Inter var font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
@@ -10,7 +13,11 @@ module.exports = {
           DEFAULT: "#06b6d4",
           dark: "#0e7490",
         },
+
         // ...
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
