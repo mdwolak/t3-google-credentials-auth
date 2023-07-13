19c17008268f3f99643b6a8a9f648b55eb68bb80
mdwolak <mdwolak@googlemail.com>
2023-07-13 23:28:20 +0100

    Prevent sensitive user details from leaking (defaultUserSelect)

--- src/server/services/user.ts
+++ src/server/services/user.ts
@@ -1,10 +1,19 @@
-import type { Prisma, User } from "@prisma/client";
+import { Prisma, type User } from "@prisma/client";
 
 import { prisma } from "~/server/db";
 
+export const defaultUserSelect = Prisma.validator<Prisma.UserSelect>()({
+  id: true,
+  //email: true,
+  name: true,
+  image: true,
+  updatedAt: true,
+  role: true,
+});
+
 export const findFirst = async (
   where: Partial<Prisma.UserWhereInput>,
-  select?: Prisma.UserSelect
+  select: Prisma.UserSelect = defaultUserSelect
 ) => {
   return (await prisma.user.findFirst({
     where,
@@ -14,7 +23,7 @@ export const findFirst = async (
 
 export const findUnique = async (
   where: Prisma.UserWhereUniqueInput,
-  select?: Prisma.UserSelect
+  select: Prisma.UserSelect = defaultUserSelect
 ) => {
   return (await prisma.user.findUnique({
     where,
@@ -26,18 +35,14 @@ export const create = async (input: Prisma.UserCreateInput) => {
   //IMPROVE: do not return sensitive data exemplar-creation
   return (await prisma.user.create({
     data: input,
-    select: {
-      email: true,
-      name: true,
-      image: true,
-    },
+    select: defaultUserSelect,
   })) as User;
 };
 
 export const update = async (
   where: Partial<Prisma.UserWhereUniqueInput>,
   data: Prisma.UserUpdateInput,
-  select?: Prisma.UserSelect
+  select: Prisma.UserSelect = defaultUserSelect
 ) => {
   return (await prisma.user.update({ where, data, select })) as User;
 };
