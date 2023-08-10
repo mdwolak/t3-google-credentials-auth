6418e48ffa6af1f015bd11f6c490c1d03cf7248e
mdwolak <mdwolak@googlemail.com>
2023-08-10 23:20:24 +0100

    Seed admin user

---
+++ prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+import { hash } from "bcrypt";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const password = await hash("adminpassword", 10);
+
+  await prisma.user.create({
+    data: {
+      email: "admin@example.com",
+      password: password,
+      role: "ADMIN",
+    },
+  });
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
