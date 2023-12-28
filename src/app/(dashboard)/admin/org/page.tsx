866c49481e56683447667c549267f5d9fe7c1f48
mdwolak <mdwolak@googlemail.com>
2023-12-28 23:59:25 +0000

    feat: Add TabList

--- src/app/(dashboard)/admin/org/page.tsx
+++ src/app/(dashboard)/admin/org/page.tsx
@@ -1,8 +1,19 @@
+import { TabList } from "~/components/navbars/TabList";
+
 import { OrganisationList } from "./organisation-list";
+
+const tabs = [
+  { name: "Applied", href: "#", count: "52", current: false },
+  { name: "Phone Screening", href: "#", count: "6", current: false },
+  { name: "Interview", href: "#", count: "4", current: true },
+  { name: "Offer", href: "#", current: false },
+  { name: "Disqualified", href: "#", current: false },
+];
 
 export default async function OrganisationListPage() {
   return (
     <>
+      <TabList tabs={tabs} />
       <OrganisationList />
     </>
   );
---
+++ src/components/navbars/TabList.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState } from "react";
+
+import { Tab } from "@headlessui/react";
+import classNames from "classnames";
+
+type Tab = {
+  name: string;
+  href: string;
+  count?: string;
+  current?: boolean;
+};
+
+export function TabList({ tabs }: { tabs: Tab[] }) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  return (
+    <div className="border-b border-gray-200">
+      <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
+        <Tab.List className="-mb-px flex space-x-8" aria-label="Tabs" as="nav">
+          {tabs.map((tab, index) => (
+            <Tab
+              key={tab.name}
+              className={classNames(
+                index == selectedIndex
+                  ? "border-indigo-500 text-indigo-600"
+                  : "border-transparent text-gray-500 hover:border-gray-200 hover:text-gray-700",
+                "flex whitespace-nowrap border-b-2 px-1 py-4 text-sm font-medium",
+              )}
+              aria-current={tab.current ? "page" : undefined}>
+              {tab.name}
+              {tab.count ? (
+                <span
+                  className={classNames(
+                    tab.current ? "bg-indigo-100 text-indigo-600" : "bg-gray-100 text-gray-900",
+                    "ml-3 hidden rounded-full px-2.5 py-0.5 text-xs font-medium md:inline-block",
+                  )}>
+                  {tab.count}
+                </span>
+              ) : null}
+            </Tab>
+          ))}
+        </Tab.List>
+      </Tab.Group>
+    </div>
+  );
+}
