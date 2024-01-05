8f48a5d2dfa8862cea38d1a89fc239bdbcf107a9
mdwolak <mdwolak@googlemail.com>
2024-01-05 14:51:07 +0000

    chore(storybook): Add console and actions addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,12 @@ import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
+    "@storybook/addon-console",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm"
+    "@storybook/addon-links",
+    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-onboarding",
   ],
   framework: {
     name: "@storybook/nextjs",
