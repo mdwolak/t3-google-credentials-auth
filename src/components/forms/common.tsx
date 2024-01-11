b856fab1acfe0b756fa1bc6b776c7aac922317da
mdwolak <mdwolak@googlemail.com>
2024-01-11 19:39:32 +0000

    feat: Add Fieldset component

--- src/components/forms/common.tsx
+++ src/components/forms/common.tsx
@@ -34,6 +34,36 @@ export function FieldError({ error }: { error: string }) {
   );
 }
 
+type FieldsetProps = {
+  disabled?: boolean;
+  title: string;
+  description?: string;
+  children: React.ReactNode;
+};
+
+export function Fieldset({ disabled, title, description, children }: FieldsetProps) {
+  return (
+    <fieldset
+      disabled={disabled}
+      className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
+      <div>
+        <h2 className="text-base font-semibold leading-7 text-gray-900">{title}</h2>
+        {description && <p className="mt-1 text-sm leading-6 text-gray-600">{description}</p>}
+      </div>
+
+      <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
+        {/* Wrap form fields with <div sm:col-span-(2,3,4,5,full)> */}
+        {children}
+      </div>
+    </fieldset>
+  );
+}
+
+/* <button type="button" className="text-sm font-semibold leading-6 text-gray-900">Cancel</button>; */
+Fieldset.Buttons = function FieldsetButtons({ children }: { children: React.ReactNode }) {
+  return <div className="mt-6 flex items-center justify-end gap-x-6">{children}</div>;
+};
+
 export const getFieldError = (errors: FieldErrors, fieldName: string) => {
   return get(errors, fieldName);
 };
